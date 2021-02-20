d91d54d87750b6b0302c0c994f6da9b184d1e1b8
fmatheus21 <fernando.matheuss@hotmail.com>
2021-02-20 20:32:43 -0300

    Implementação do Repository

---
+++ api/src/main/java/com/fmatheus/app/model/persistence/repository/RepositoryFilter.java
@@ -0,0 +1,17 @@
+package com.fmatheus.app.model.persistence.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author fmatheus
+ */
+public class RepositoryFilter {
+  
+    @Getter
+    @Setter
+    private String nome;  
+    
+
+}
